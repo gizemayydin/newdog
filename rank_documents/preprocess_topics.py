4679ecfcec66e36c2df2c864be5f96c92ec42267
lnovakovi <loranovakovic@gmail.com>
2019-12-18 16:47:33 +0100

    Fixed bugs

--- rank_documents/preprocess_topics.py
+++ rank_documents/preprocess_topics.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle
 import time
 import sys
-sys.path.append("..")
+sys.path.append(".")
 from src.main.python.topic_reader import TopicReader
 
 
@@ -13,6 +13,6 @@ from src.main.python.topic_reader import TopicReader
 if __name__ == "__main__" :
 	path_to_topics = sys.argv[1]
 	tr = TopicReader(path_to_topics)
-	topics = tr.return_topics()
+	topics = tr.get_topics()
 	with open('topics.data', 'wb') as filehandle:
 	    pickle.dump(topics, filehandle)
